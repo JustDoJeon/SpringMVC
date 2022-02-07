629977b36a00df3ad91a71484edff98b008210fa
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2022-02-07 13:39:22 +0900

    Servlet 으로 request, response 구성해보기

--- src/main/java/mvc/spring/Application.java
+++ src/main/java/mvc/spring/Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan //내 패키지 포함 밑에 경로 서블릿 다 찾아서 자동등록
+
+//내 패키지 포함 밑에 경로 서블릿 다 찾아서 자동등록
+@ServletComponentScan
 @SpringBootApplication
 public class Application {
 
