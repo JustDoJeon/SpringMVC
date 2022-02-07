8688982d572ca58382f568fcd68b405afd9f9697
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2022-02-07 22:16:08 +0900

    Spring 구조 직접 쳐보며 완성해보기

--- src/main/java/mvc/spring/Application.java
+++ src/main/java/mvc/spring/Application.java
@@ -3,6 +3,8 @@ package mvc.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
 //내 패키지 포함 밑에 경로 서블릿 다 찾아서 자동등록
--- src/main/java/mvc/spring/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/mvc/spring/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -1,14 +1,15 @@
-package mvc.spring.springmvc.v1;
+package mvc.spring.web.springmvc.v1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping
 public class SpringMemberFormControllerV1 {
 
     @RequestMapping("/springmvc/v1/members/new-form")
     public ModelAndView process() {
-        return  new ModelAndView("new-form");
+        return new ModelAndView("new-form");
     }
 }
